e3f94710cd1a9b0a0777dc149cd5427ded56ab79
eironasas <eironas.grigas@gmail.com>
2019-09-25 09:22:27 +0300

    1 laboras, buttonclick makes changes to textview

--- app/src/main/java/com/example/a1laboras/MainActivity.java
+++ app/src/main/java/com/example/a1laboras/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a1laboras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,9 +17,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    @SuppressLint("SetTextI18n")
     public void buttonClick(View v)
     {
-        TextView tv = (TextView)findViewById(R.id.textView1);
+        TextView tv = findViewById(R.id.textView);
         tv.setText("Welcome to android");
     }
 }
